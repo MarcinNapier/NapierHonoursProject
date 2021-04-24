9e05555cf83b8d4a9884ec508c3e55f8b4e3ce19
MarcinNapier <40445464@live.napier.ac.uk>
2021-04-24 14:09:27 +0200

    first commit

--- main.py
+++ main.py
@@ -174,5 +174,5 @@ def logout():
 
 
 if __name__ == "__main__":
-    from waitress import serve
-    serve(app, host="0.0.0.0", port=8080)
+    app.secret_key = os.urandom(12)
+    app.run(debug=True)
